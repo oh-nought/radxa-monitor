a781c3917014a0ad7f82e6d1f06b4616c4732d61
c-rti <isntsanchez@gmail.com>
2025-12-28 17:54:06 -0500

    first commit

---
+++ metric_reader.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <thread>
+#include <string>
+using namespace std;
+
+int main() {
+    for (int i = 0; i < 3; i++) {
+        string cpu_data;
+        ifstream ifile("/proc/stat");
+        std::getline(ifile, cpu_data);
+        cout << cpu_data;
+        this_thread::sleep_for(chrono::seconds(3));
+    }
+}
