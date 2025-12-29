76d08cc2f4c85a6c3ea65e7ff22e0c146de13231
c-rti <isntsanchez@gmail.com>
2025-12-29 13:30:26 -0500

    update

--- main.cpp
+++ main.cpp
@@ -13,12 +13,12 @@ int main() {
         auto temp = readings.read_temperature();
         auto memory = readings.read_memory();
     
-        cout << "CPU Usage: " << fixed << setprecision << cpu << endl;
+        cout << "CPU Usage: " << fixed << setprecision(2) << cpu << endl;
         cout << "Temperature in C: " << temp.temp_c << endl;
         cout << "Temperature in F: " << temp.temp_f << endl;
-        cout << "Total Memory: " << fixed << setprecision << memory.total_memory << endl;
-        cout << "Used Memory: " << fixed << setprecision << memory.used_memory << endl;
-        cout << "Cached Memory: " << fixed << setprecision << memory.cached_memory << endl;
+        cout << "Total Memory: " << fixed << setprecision(2) << memory.total_memory << endl;
+        cout << "Used Memory: " << fixed << setprecision(2) << memory.used_memory << endl;
+        cout << "Cached Memory: " << fixed << setprecision(2) << memory.cached_memory << endl;
         cout << "\n" << endl;
         
         this_thread::sleep_for(chrono::seconds(3));
