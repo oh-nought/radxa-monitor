c12acda1cc7b45257aee3ff3a9b477bd95b5323d
c-rti <isntsanchez@gmail.com>
2026-01-03 13:19:34 -0500

    update styling

--- web/static/monitor.js
+++ web/static/monitor.js
@@ -25,7 +25,7 @@ class Monitor {
             const svg = container.querySelector('svg');
             svg.removeAttribute('width');
             svg.removeAttribute('height');
-            svg.setAttribute('viewBox', '0 0 1870 960');
+            svg.setAttribute('viewBox', '0 0 935 480');
             svg.setAttribute('preserveAspectRatio', 'xMidYMid meet');
         } catch (error) {
             console.error("Failed to load SVG:", error);
